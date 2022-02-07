ca9cd78d215651c551f58942d8d76c3154fcef72
Sepsad <sepehrsdp@gmail.com>
2022-02-07 16:52:53 +0330

    contest updated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/", function (request, response) {
 });
 
 
-var server = app.listen(process.env.PORT || 5000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
     console.log("listening to port %d", server.address().port);
     
